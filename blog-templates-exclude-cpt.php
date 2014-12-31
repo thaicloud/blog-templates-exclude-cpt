0dfb64e82ffaf6731ddfaaff1b27101d973affcf
Michael Jordan <chiangmaiguy@gmail.com>
2014-12-31 10:53:22 -0500

    General reformatting

--- blog-templates-exclude-cpt.php
+++ blog-templates-exclude-cpt.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: New Blog Templates: Exclude Custom Post Type
 Description: Choose which post types should not be copied from the template
@@ -8,7 +9,7 @@ Author URI: http://knowmike.com/
 */
 
 class MJ_Blog_Templates_Exclude_Post_Type {
-	
+
 	/**
 	 * @var List of post_types not permitted to copy
 	 */
@@ -18,17 +19,17 @@ class MJ_Blog_Templates_Exclude_Post_Type {
 	 * @var Check if we are on settings page
 	 */
 	private $exclude_cpt_settings_check;
-	
+
 	/**
-	 * Constructor 
+	 * Constructor
 	 */
 	public function __construct() {
-		
+
 		// Add the super admin page
 		add_action( 'network_admin_menu', array( $this, 'network_admin_page' ) );
 
 		// Workaround for WP Settings API bug with multisite
-		add_action( 'network_admin_edit_exclude-cpt-settings', array( $this, 'save_exclude_cpt_settings') );
+		add_action( 'network_admin_edit_exclude-cpt-settings', array( $this, 'save_exclude_cpt_settings' ) );
 
 		add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 
@@ -38,44 +39,48 @@ class MJ_Blog_Templates_Exclude_Post_Type {
 		add_action( 'wp_ajax_exclude_cpt_ajax_populate', array( $this, 'exclude_cpt_ajax_populate' ) );
 
 		// Hook into blog template and skip copying of post types
-		add_filter( 'blog_templates-process_row' , array( $this, 'skip_post_types' ), 11, 2 );
+		add_filter( 'blog_templates-process_row', array( $this, 'skip_post_types' ), 11, 2 );
 
 	}
-
-   /**
-    * Enqueue scripts for ajax handling / nonce
-    *
-    */
-	function admin_enqueue_scripts( $hook ){		
-		if ( $hook != $this->exclude_cpt_settings_check )
-			return;
-		wp_enqueue_script( 'blog-templates-exclude-cpt-ajax', plugin_dir_url( __FILE__  ). 'js/blog-templates-exclude-cpt.js', array( 'jquery' ) );
-		wp_localize_script( 'blog-templates-exclude-cpt-ajax', 'bte_vars', array(
-				'bte_nonce' => wp_create_nonce( 'bte_nonce' )
-			) 
-		);
-	}
-	
-   /**
-    * Adds the network admin menu item
-    *
-    */
-    function network_admin_page() {
-		$this->exclude_cpt_settings_check = add_submenu_page( 'settings.php', __( 'Exclude CPT', $this->localization_domain ), __( 'Exclude CPT', $this->localization_domain ), 'administrator', basename(__FILE__), array(&$this,'admin_options_page'));
-    }
 
 	/**
-    * 	Display settings page
-    *
-    */
-    function admin_options_page() {
+	 * Enqueue scripts for ajax handling / nonce
+	 *
+	 */
+	function admin_enqueue_scripts( $hook ) {
+		if ( $hook != $this->exclude_cpt_settings_check ) {
+			return;
+		}
+		wp_enqueue_script( 'blog-templates-exclude-cpt-ajax', plugin_dir_url( __FILE__ ) . 'js/blog-templates-exclude-cpt.js', array( 'jquery' ) );
+		wp_localize_script( 'blog-templates-exclude-cpt-ajax', 'bte_vars', array(
+				'bte_nonce' => wp_create_nonce( 'bte_nonce' )
+			)
+		);
+	}
 
-    	global $pagenow;
-		if ( $pagenow != 'settings.php' && $_GET['page'] != 'blog-templates-exclude-cpt.php') {
+	/**
+	 * Adds the network admin menu item
+	 *
+	 */
+	function network_admin_page() {
+		$this->exclude_cpt_settings_check = add_submenu_page( 'settings.php', __( 'Exclude CPT', $this->localization_domain ), __( 'Exclude CPT', $this->localization_domain ), 'administrator', basename( __FILE__ ), array(
+				&$this,
+				'admin_options_page'
+			) );
+	}
+
+	/**
+	 *    Display settings page
+	 *
+	 */
+	function admin_options_page() {
+
+		global $pagenow;
+		if ( $pagenow != 'settings.php' && $_GET['page'] != 'blog-templates-exclude-cpt.php' ) {
 			return;
 		}
 
-    	echo '<div class="wrap">';
+		echo '<div class="wrap">';
 		echo '<h2>Exclude Custom Post Types</h2>';
 
 		echo 'This plugin extends upon the incredible <a href="https://premium.wpmudev.org/project/new-blog-template/" target="_blank">New Blog Template</a>
@@ -84,29 +89,29 @@ class MJ_Blog_Templates_Exclude_Post_Type {
 		echo '<form name="exclude_cpt_form" method="post" action="edit.php?action=exclude-cpt-settings">';
 
 		// Get 'New Blog Template' plugin options
-		$new_blog_templates = get_site_option('blog_templates_options');
+		$new_blog_templates = get_site_option( 'blog_templates_options' );
 
 		$templates = array();
-		foreach ($new_blog_templates['templates'] as $key=>$template) {
+		foreach ( $new_blog_templates['templates'] as $key => $template ) {
 			// Only list templates that are set to copy posts
-			if ( in_array( 'posts', $template['to_copy'] ) ){
-				$blog_id = $template['blog_id'];
-				$templates[$blog_id] = $template['name'];
+			if ( in_array( 'posts', $template['to_copy'] ) ) {
+				$blog_id               = $template['blog_id'];
+				$templates[ $blog_id ] = $template['name'];
 			}
-        }
+		}
 
-        if ( $templates != array() ) {
+		if ( $templates != array() ) {
 			echo "<p><select name='template-exclude-cpt-settings' id='template-exclude-cpt-settings'>";
-        	echo "<option value=''>Please select a template</option>" ;
-        	foreach ( $templates as $key=>$value ) {
-        	    echo "<option value='$key'>$value</option>";
-        	}
-        	echo '</select> ';
-        	echo '<img src="'.admin_url( '/images/wpspin_light.gif' ).'" class="waiting" id="bte-loading" style="display: none;">';
-        	echo '</p>';
-        }else{
-        	echo '<p>Please ensure that you have created one or more blog templates (that are setup to copy posts) using the "New Blog Template" plugin.</p>';
-        }
+			echo "<option value=''>Please select a template</option>";
+			foreach ( $templates as $key => $value ) {
+				echo "<option value='$key'>$value</option>";
+			}
+			echo '</select> ';
+			echo '<img src="' . admin_url( '/images/wpspin_light.gif' ) . '" class="waiting" id="bte-loading" style="display: none;">';
+			echo '</p>';
+		} else {
+			echo '<p>Please ensure that you have created one or more blog templates (that are setup to copy posts) using the "New Blog Template" plugin.</p>';
+		}
 		?>
 		<div id="exclude-cpt-ajax">
 
@@ -114,71 +119,72 @@ class MJ_Blog_Templates_Exclude_Post_Type {
 		<?php
 		echo '</form>';
 		echo '</div>';
-    }
+	}
 
 	/**
-    * 	Called via Ajax- using new source blog id, display all post types as checkboxes
-    *
-    */
-	function exclude_cpt_ajax_populate( ){
+	 *    Called via Ajax- using new source blog id, display all post types as checkboxes
+	 *
+	 */
+	function exclude_cpt_ajax_populate() {
 
-		if( !isset( $_POST['bte_nonce'] ) || !wp_verify_nonce( $_POST['bte_nonce'], 'bte_nonce' ) )
+		if ( ! isset( $_POST['bte_nonce'] ) || ! wp_verify_nonce( $_POST['bte_nonce'], 'bte_nonce' ) ) {
 			die( 'Permissions check failed' );
+		}
 
-		$blog_id = $_POST['blog_id'];
-		$settings = (array) get_site_option( 'exclude-cpt-settings-'.$blog_id );
+		$blog_id  = $_POST['blog_id'];
+		$settings = (array) get_site_option( 'exclude-cpt-settings-' . $blog_id );
 
-		echo '<p>Please choose which post types you DO NOT wish to be included in the copy.</p>';		
-		echo '<input type="hidden" name="bte-option-name" value="exclude-cpt-settings-'.$blog_id.'" >';
+		echo '<p>Please choose which post types you DO NOT wish to be included in the copy.</p>';
+		echo '<input type="hidden" name="bte-option-name" value="exclude-cpt-settings-' . $blog_id . '" >';
 
 		global $switched;
-   		switch_to_blog( $blog_id );
-   		global  $wpdb;
+		switch_to_blog( $blog_id );
+		global $wpdb;
 
-   		$post_types = 'SELECT post_type
+		$post_types = 'SELECT post_type
              FROM ' . $wpdb->posts . '                
              GROUP BY post_type';
-        $post_types = $wpdb->get_results($post_types, ARRAY_A);
+		$post_types = $wpdb->get_results( $post_types, ARRAY_A );
 
-    	restore_current_blog();
-    	echo '<table>';
-    	foreach ( $post_types as $post_type ){
-    		$post_type = $post_type['post_type'];
-    		echo "<tr><td> $post_type </td><td> <input type='checkbox' id='exclude-cpt-settings-.$post_type' name='exclude-cpt-settings-$post_type' ". checked( $settings[$post_type], 'on', false ) ."></td></tr>";
-    	}
-    	echo '</table>';
+		restore_current_blog();
+		echo '<table>';
+		foreach ( $post_types as $post_type ) {
+			$post_type = $post_type['post_type'];
+			echo "<tr><td> $post_type </td><td> <input type='checkbox' id='exclude-cpt-settings-.$post_type' name='exclude-cpt-settings-$post_type' " . checked( $settings[ $post_type ], 'on', false ) . "></td></tr>";
+		}
+		echo '</table>';
 
-    	submit_button( 'Save Changes' );
+		submit_button( 'Save Changes' );
 
-    	die();
+		die();
 	}
 
 	/**
 	 * Save options form on submission
 	 *
 	 */
-	function save_exclude_cpt_settings( ){
+	function save_exclude_cpt_settings() {
 
 		$allowed_post_types = array();
-		foreach ( $_POST as $key => $val ){
-			$count = 0;
-			$post_type = str_replace("exclude-cpt-settings-", "", $key, $count);
+		foreach ( $_POST as $key => $val ) {
+			$count     = 0;
+			$post_type = str_replace( "exclude-cpt-settings-", "", $key, $count );
 
-			if ( $count == 1 ){
-				$allowed_post_types[$post_type] = 'on';
+			if ( $count == 1 ) {
+				$allowed_post_types[ $post_type ] = 'on';
 			}
 		}
-		
-		if ( count( $allowed_post_types ) != 0 ){
+
+		if ( count( $allowed_post_types ) != 0 ) {
 			$this->exclude_cpt_settings = $allowed_post_types;
 			update_site_option( $_POST['bte-option-name'], $allowed_post_types );
 		}
 
 		wp_redirect(
-    		add_query_arg(
-       		 array( 'page' => 'blog-templates-exclude-cpt.php', 'updated' => 'true' ),
-        	(is_multisite() ? network_admin_url( 'admin.php' ) : admin_url( 'admin.php' ))
- 			)
+			add_query_arg(
+				array( 'page' => 'blog-templates-exclude-cpt.php', 'updated' => 'true' ),
+				( is_multisite() ? network_admin_url( 'admin.php' ) : admin_url( 'admin.php' ) )
+			)
 		);
 		exit;
 	}
@@ -187,21 +193,22 @@ class MJ_Blog_Templates_Exclude_Post_Type {
 	 * Run just before copy process starts; check which template is running & set variable
 	 *
 	 */
-	function check_source_blog_pre_copy( $table, $blog_id ){
-		$this->exclude_cpt_settings = (array) get_site_option( 'exclude-cpt-settings-'.$blog_id );
+	function check_source_blog_pre_copy( $table, $blog_id ) {
+		$this->exclude_cpt_settings = (array) get_site_option( 'exclude-cpt-settings-' . $blog_id );
 	}
 
-    /**
-	 * The magic happens here. 
+	/**
+	 * The magic happens here.
 	 * Filter for 'New Blog Template' plugin allows us to allow/disallow copying at the last minute.
 	 *
 	 */
-	function skip_post_types( $row, $table ){
+	function skip_post_types( $row, $table ) {
 		if ( isset( $row['post_type'] ) ) {
-			if( array_key_exists( $row['post_type'] , $this->exclude_cpt_settings) ){
+			if ( array_key_exists( $row['post_type'], $this->exclude_cpt_settings ) ) {
 				return false;
 			}
 		}
+
 		return true;
 	}
 
